bc60c551e194220d984318df2b929f0758489c34
MurunGantulga <munu2014@gmail.com>
2022-01-24 11:29:53 -0600

    Part 2 is created

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -4,5 +4,6 @@ public class TestCase003 {
     public static void main(String[] args) {
         System.out.println("Conflict example");
         System.out.println("Part 1 is done");
+        System.out.println("Part 2 is done");
     }
 }
