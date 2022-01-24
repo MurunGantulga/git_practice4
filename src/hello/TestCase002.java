3ac6433f00b2d90a96528c9f7e72c9965bbe4b17
MurunGantulga <munu2014@gmail.com>
2022-01-24 10:35:16 -0600

    Part 2 is done

--- src/hello/TestCase002.java
+++ src/hello/TestCase002.java
@@ -4,5 +4,6 @@ public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("Project 2 is started");
         System.out.println("Part 1 is done");
+        System.out.println("Part 2 is done");
     }
 }
