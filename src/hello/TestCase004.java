e45354cc42e3749f70a4ea46bf97be6d34ea481a
MurunGantulga <munu2014@gmail.com>
2022-01-24 13:43:07 -0600

    part 2 is done

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -4,5 +4,6 @@ public class TestCase004 {
     public static void main(String[] args) {
         System.out.println("Invite contributors to the project");
         System.out.println("Part 1 is done");
+        System.out.println("Part 2 is done");
     }
 }
